693791b404e5ed93dc85a895d13f349f3bd018a7
Kendrick <ehsanlangroudi.kk@gmail.com>
2022-06-07 19:08:35 +0430

    Added Roadmap Section

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
