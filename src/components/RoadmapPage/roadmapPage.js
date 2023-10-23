b85e21c74ad2af17559c665fa5a18a9c3de7acb2
Sean Hardy <seanjhardy@icloud.com>
2023-10-23 12:38:17 +0100

    Change roadmap to mobile view when browser width is below 720

--- src/components/RoadmapPage/roadmapPage.js
+++ src/components/RoadmapPage/roadmapPage.js
@@ -1,7 +1,30 @@
+import {useEffect, useState} from "react";
 
 
 export const RoadmapPage = () => {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
+    const handleResize = () => {
+      setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || window.innerWidth < 720)
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="home-container">
